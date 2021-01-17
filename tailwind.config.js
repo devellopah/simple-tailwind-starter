e5dd10d0bb72b24711ea55381a506cf70161ab4a
Islam <islamibakaev@gmail.com>
2021-01-17 10:44:11 +0300

    configured  option to remove unused css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   //   removeDeprecatedGapUtilities: true,
   //   purgeLayersByDefault: true,
   // },
-  purge: [],
+  purge: ['./public/**/*.html'],
   theme: {
     extend: {},
   },
